90ceaf6cf7152649c5dbc48f754a866ba70cde06
Mayowa Falade <falademayowa240@gmail.com>
2021-11-27 22:59:34 -0500

    Setup tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -2,9 +2,21 @@ import React from 'react';
 
 function App() {
   return (
-    <div className="App">
-      <h3>Morse Code Translator</h3>
-      <p>Translates ASCII text to Morse code and vice-versa</p>
+    <div className="text-center mt-4">
+      <div>
+        <h3>Morse Code Translator</h3>
+        <p>Translates ASCII text to Morse code and vice-versa</p>
+      </div>
+
+      <div className="w-full mt-8">
+        <form>
+          <input
+            type="text"
+            placeholder="testing tailwind"
+            className="p-6 max-w-sm mx-auto bg-purple-100"
+          />
+        </form>
+      </div>
     </div>
   );
 }
